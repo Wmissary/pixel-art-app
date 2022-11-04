43cf506eacfe3742b2aae889fd1ba39042f40c1e
Wmissary <pro.wmissary@gmail.com>
2022-11-04 18:21:36 +0100

    feat: very basic version of drawing with color

---
+++ main.js
@@ -0,0 +1,55 @@
+class Tile {
+  constructor(data) {
+    this.x = data.x;
+    this.y = data.y;
+    this.color = data.color;
+    this.width = 1;
+    this.height = 1;
+  }
+  draw(ctx) {
+    ctx.fillStyle = this.color;
+    ctx.fillRect(this.x, this.y, this.width, this.height);
+  }
+  clear(ctx) {
+    ctx.clearRect(this.x, this.y, this.width, this.height);
+  }
+}
+
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+
+let click = false;
+
+canvas.onmousedown = (e) => {
+  const color = document.getElementById("color").value;
+
+  const x = Math.floor(
+    e.offsetX / (parseInt(getComputedStyle(canvas).width) / 16)
+  );
+  const y = Math.floor(
+    e.offsetY / (parseInt(getComputedStyle(canvas).width) / 16)
+  );
+  const tile = new Tile({ x, y, color });
+  tile.draw(ctx);
+
+  click = true;
+};
+
+canvas.onmousemove = (e) => {
+  if (click) {
+    const color = document.getElementById("color").value;
+
+    const x = Math.floor(
+      e.offsetX / (parseInt(getComputedStyle(canvas).width) / 16)
+    );
+    const y = Math.floor(
+      e.offsetY / (parseInt(getComputedStyle(canvas).width) / 16)
+    );
+    const tile = new Tile({ x, y, color });
+    tile.draw(ctx);
+  }
+};
+
+canvas.onmouseup = () => {
+  click = false;
+};
