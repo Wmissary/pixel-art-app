c0342441319232d0359dfbb04a99385d7c9babce
Wmissary <pro.wmissary@gmail.com>
2022-11-25 18:45:23 +0100

    feat: add zoom / unzoom canvas & add translate canvas

--- main.js
+++ main.js
@@ -137,4 +137,44 @@ document.addEventListener("DOMContentLoaded", () => {
 
   // add EventListeners to Export Buttons
   exportToPngEventListener(canvas);
+
+  // add EventListeners to Canvas container
+
+  const canvasContainer = document.getElementById("canvas-container");
+
+  // move canvas on mouse drag
+  let isDragging = false;
+  let lastX = 0;
+  let lastY = 0;
+  canvasContainer.addEventListener("mousedown", (e) => {
+    if (e.buttons !== 4) return;
+    isDragging = true;
+    lastX = e.clientX;
+    lastY = e.clientY;
+  });
+  canvasContainer.addEventListener("mousemove", (e) => {
+    if (!isDragging) return;
+    const style = getComputedStyle(canvas.element);
+    const matrix = new DOMMatrix(style.transform);
+    const x = e.clientX - lastX;
+    const y = e.clientY - lastY;
+    canvas.element.style.transform = `translate(${matrix.e + x}px, ${
+      matrix.f + y
+    }px) scale(${matrix.a})`;
+    lastX = e.clientX;
+    lastY = e.clientY;
+  });
+  canvasContainer.addEventListener("mouseup", () => {
+    isDragging = false;
+  });
+
+  // zoom canvas on mouse wheel
+  canvasContainer.addEventListener("wheel", (e) => {
+    const style = getComputedStyle(canvas.element);
+    const matrix = new DOMMatrix(style.transform);
+    const zoom = e.deltaY > 0 ? 0.9 : 1.1;
+    canvas.element.style.transform = `translate(${matrix.e}px, ${
+      matrix.f
+    }px) scale(${Math.round((matrix.a * zoom + Number.EPSILON) * 10) / 10})`;
+  });
 });
